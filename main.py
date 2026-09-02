d0a1902ffaad853e927992191b1435bcbef5798f
Z1058 <omarnairoukh10@gmail.com>
2026-09-02 03:59:47 +0100

    Add Dijksta pathfinding, weighted cells, search animations and results tracking

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import tkinter as tk
 from customtkinter import CTk, CTkOptionMenu, CTkFrame, CTkLabel, CTkButton, CTkEntry, CTkCheckBox
 from collections import deque
 import time
+import heapq
 
 walls = set()
-cellrectangles = dict()
+weights = {}
+cellrectangles = {}
 start = None
 end = None
 mode = "wall"
 algo = "BFS"
 resultsvalid = False
+animate = True
 
 def creategrid():
     for row in range(rows):
@@ -40,6 +43,8 @@ def placecell(event):
             invalidate()
         if (row, column) in walls:
             walls.remove((row, column))
+        if (row, column) in weights:
+            del weights[(row, column)]
         if start is not None:
             oldrectangle = cellrectangles[start]
             canvas.itemconfig(oldrectangle, fill = "white")
@@ -51,6 +56,8 @@ def placecell(event):
             return
         if (row, column) in walls:
             walls.remove((row, column))
+        if (row, column) in weights:
+            del weights[(row, column)]
         if (row, column) != end:
             invalidate()
         if end is not None:
@@ -58,6 +65,18 @@ def placecell(event):
             canvas.itemconfig(oldrectangle, fill = "white")
         end = (row, column)
         canvas.itemconfig(rectangleid, fill = "red")
+
+    elif mode == "weight":
+        if (row, column) == start or (row, column) == end:
+            return
+        invalidate()
+
+        if (row, column) in walls:
+            walls.remove((row, column))
+
+        weights[(row, column)] = 5
+        canvas.itemconfig(rectangleid, fill = "#ff6e2b")
+
 
     elif mode == "erase":
         removewall(event)
@@ -75,6 +94,11 @@ def addwall(event):
     rectangleid = cellrectangles[(row, column)]
     if (row, column) not in walls and (row, column) != start and (row, column) != end:
         invalidate()
+
+        if (row, column) in weights:
+            del weights[(row, column)]
+
+
         canvas.itemconfig(rectangleid, fill = "black")
         walls.add((row, column))
     #    print("Row:", row, "Column", column)
@@ -129,6 +153,7 @@ def applygridsize():
     canvas.delete("all")
     cellrectangles.clear()
     walls.clear()
+    weights.clear()
 
     start = None
     end = None
@@ -151,11 +176,19 @@ def removewall(event):
             invalidate()
             canvas.itemconfig(rectangleid, fill = "white")
             walls.remove((row, column))
+    elif (row, column) in weights:
+        invalidate()
+        canvas.itemconfig(rectangleid, fill = "white")
+        del weights[(row, column)]
 
 def clearsearch():
     for cell in cellrectangles:
-        if cell not in walls and cell != start and cell != end:
-            rectangleid = cellrectangles[cell]
+        rectangleid = cellrectangles[cell]
+
+        if cell in weights:
+            canvas.itemconfig(rectangleid, fill = "#ff6e2b")
+
+        elif cell not in walls and cell != start and cell != end:
             canvas.itemconfig(rectangleid, fill = "white")
 
 def clearall():
@@ -166,6 +199,7 @@ def clearall():
         rectangleid = cellrectangles[cell]
         canvas.itemconfig(rectangleid, fill = "white")
     walls.clear()
+    weights.clear()
     invalidate()
     clearsearch()
 
@@ -183,16 +217,11 @@ def runalgo():
     clearsearch()
     if algo == "BFS":
         success = bfs()
-    if success:
-        resultsvalid = True
-
-    if algo == "Dijkstra's":
+    elif algo == "Dijkstra's":
         success = dijkstra()
-    if success:
-        resultsvalid = True
-
-    if algo == "A*":
+    elif algo == "A*":
         success = a()
+
     if success:
         resultsvalid = True
 
@@ -200,14 +229,17 @@ def clearresults():
     bfsvisit.configure(text = "-")
     bfspath.configure(text = "-")
     bfstime.configure(text = "-")
+    bfscost.configure(text = "-")
 
     dijkvisit.configure(text = "-")
     dijkpath.configure(text = "-")
     dijktime.configure(text = "-")
+    dijkcost.configure(text = "-")
 
     avisit.configure(text = "-")
     apath.configure(text = "-")
     atime.configure(text = "-")
+    acost.configure(text = "-")
 
 def invalidate():
     global resultsvalid
@@ -226,8 +258,10 @@ def bfs():
     visited = {start}
     camefrom = {}
     path = []
+    visitorder = []
     while queue:
         current = queue.popleft()
+        visitorder.append(current)
         nodesexp += 1
         if current == end:
             break
@@ -249,25 +283,163 @@ def bfs():
         current = camefrom[current]
     path.append(current)
     path.reverse()
+    pathcost = 0
+    for cell in path[1:]:
+        pathcost += weights.get(cell, 1)
     endtime = time.perf_counter()
     exetime = endtime - starttime
+    if exetime > 0.1:
+        multiplier = 100
+    elif exetime > 0.05:
+        multiplier = 500
+    elif exetime > 0.01:
+        multiplier = 750
+    elif exetime > 0.005:
+        multiplier = 1250
+    elif exetime > 0.0025:
+        multiplier = 1500
+    elif exetime > 0.001:
+        multiplier = 2500
+    else:
+        multiplier = 5000
+    aniduration = exetime * multiplier
+    anidurationprint = round(aniduration, 2)
+    delay = (aniduration * 1000) / len(visitorder)
     bfsvisit.configure(text = nodesexp)
     bfspath.configure(text = len(path) - 1)
     bfstime.configure(text = f"{exetime * 1000:.3f} ms")
+    bfscost.configure(text = pathcost)
     print(path)
-    feedback.configure(text = "Path found!")
+    if animate is True:
+        feedback.configure(text = f"Path found! Animation duration: {anidurationprint}s ({multiplier}x slower)")
+        animatevisited(visitorder, path, delay)
+    else:
+        feedback.configure(text = "Path found!")
+        showpath(path)
 
+    return True
+
+def dijkstra():
+    starttime = time.perf_counter()
+    nodesexp = 0
+    visitorder = []
+    distances = {}
+    camefrom = {}
+    for cell in cellrectangles:
+        if cell not in walls:
+            distances[cell] = float("inf")
+    distances[start] = 0
+    priorityqueue = [(0, start)]
+    while priorityqueue:
+        currentdist, current = heapq.heappop(priorityqueue)
+        if currentdist > distances[current]:
+            continue
+        visitorder.append(current)
+        nodesexp += 1
+        if current == end:
+            break
+        row, column = current
+        neighbours = [(row - 1, column), (row + 1, column), (row, column - 1), (row, column + 1)]
+        for neighbour in neighbours:
+            nrow, ncolumn = neighbour
+            if 0 <= nrow < rows and 0 <= ncolumn < columns:
+                if neighbour not in walls:
+                    stepcost = weights.get(neighbour, 1)
+                    newdistance = currentdist + stepcost
+                    if distances[neighbour] > newdistance:
+                        distances[neighbour] = newdistance
+                        camefrom[neighbour] = current
+                        heapq.heappush(priorityqueue,(newdistance, neighbour))
+    path = []
+    current = end
+    if end not in camefrom:
+        feedback.configure(text = "No path found!")
+        return False
+    while current != start:
+        path.append(current)
+        current = camefrom[current]
+    path.append(current)
+    path.reverse()
+    endtime = time.perf_counter()
+    exetime = endtime - starttime
+    if exetime > 0.1:
+        multiplier = 100
+    elif exetime > 0.05:
+        multiplier = 500
+    elif exetime > 0.01:
+        multiplier = 750
+    elif exetime > 0.005:
+        multiplier = 1250
+    elif exetime > 0.0025:
+        multiplier = 1500
+    elif exetime > 0.001:
+        multiplier = 2500
+    else:
+        multiplier = 5000
+    aniduration = exetime * multiplier
+    anidurationprint = round(aniduration, 2)
+    delay = (aniduration * 1000) / len(visitorder)
+    dijkvisit.configure(text = nodesexp)
+    dijkpath.configure(text = len(path) - 1)
+    dijktime.configure(text = f"{exetime * 1000:.3f} ms")
+    dijkcost.configure(text = distances[end])
+    print(path)
+    if animate is True:
+        feedback.configure(text = f"Path found! Animation duration: {anidurationprint}s ({multiplier}x slower)")
+        animatevisited(visitorder, path, delay)
+    else:
+        feedback.configure(text = "Path found!")
+        showpath(path)
+    return True
+
+def a():
+    starttime = time.perf_counter()
+    nodesexp = 0
+    visitorder = []
+    distances = {}
+    camefrom = {}
+
+    for cell in cellrectangles:
+        if cell not in walls:
+            distances[cell] = float("inf")
+
+    distances[start] = 0
+    priorityqueue = [(heuristic(start), 0, start)]
+    while priorityqueue:
+        currentf, currentdist, current = heapq.heappop(priorityqueue)
+        if currentdist
+
+def heuristic(cell):
+    row, column = cell
+    endrow, endcolumn = end
+    manhattandist = abs(endrow - row) + abs(endcolumn - column)
+    return manhattandist
+
+def setanimation():
+    global animate
+    animate = bool(anicheckbox.get())
+
+def animatevisited(visitorder, path, delay, index = 0):
+    if index >= len(visitorder):
+        showpath(path)
+        return
+    cell = visitorder[index]
+
+    if cell != start and cell != end:
+        rectangleid = cellrectangles[cell]
+
+        if cell in weights:
+            canvas.itemconfig(rectangleid, fill = "#B98565")
+        else:
+            canvas.itemconfig(rectangleid, fill = "#00c3ff")
+
+    window.after(int(delay), lambda: animatevisited(visitorder, path, delay, index + 1))
+
+def showpath(path):
     for cell in path:
         if cell != start and cell != end:
             rectangleid = cellrectangles[cell]
             canvas.itemconfig(rectangleid, fill = "yellow")
-    return True
-
-def dijkstra():
-    print("no")
-
-def a():
-    print("no2")
 
 window = CTk()
 window.grid_rowconfigure(0, weight = 1)
@@ -317,7 +489,7 @@ gfeedback.pack(pady = (0,0))
 dmode = CTkLabel(controlpanel, text = "Drawing mode", font = ("Arial", 12), fg_color = "transparent")
 dmode.pack(pady = (20,10))
 
-modemenu = CTkOptionMenu(controlpanel, values = ["wall", "start", "end", "erase"], command = setmode)
+modemenu = CTkOptionMenu(controlpanel, values = ["wall", "start", "end", "weight", "erase"], command = setmode)
 modemenu.set(mode)
 modemenu.pack(padx = 20, pady = (0,20))
 
@@ -327,6 +499,10 @@ algoselect.pack(pady = (20,10))
 algomenu = CTkOptionMenu(controlpanel, values = ["BFS", "Dijkstra's", "A*"], command = setalgo)
 algomenu.set(algo)
 algomenu.pack(padx = 20, pady = (0,20))
+
+anicheckbox = CTkCheckBox(controlpanel, text = "Animate search", command = setanimation)
+anicheckbox.select()
+anicheckbox.pack(padx = 20, pady = 10)
 
 runbutton = CTkButton(controlpanel, text = "Run", command = runalgo, fg_color="yellow", text_color="black")
 runbutton.pack(padx = 20, pady = 10)
@@ -344,6 +520,7 @@ CTkLabel(resultsframe, text = "Algorithm").grid(row = 1, column = 0, padx = 4)
 CTkLabel(resultsframe, text = "Nodes visited").grid(row = 1, column = 1, padx = 4)
 CTkLabel(resultsframe, text = "Path").grid(row = 1, column = 2, padx = 4)
 CTkLabel(resultsframe, text = "Time taken").grid(row = 1, column = 3, padx = 4)
+CTkLabel(resultsframe, text = "Cost").grid(row = 1, column = 4, padx = 4)
 
 CTkLabel(resultsframe, text = "BFS").grid(row = 2, column = 0, padx = 4)
 CTkLabel(resultsframe, text = "Dijkstra's").grid(row = 3, column = 0, padx = 4)
@@ -355,7 +532,8 @@ bfspath = CTkLabel(resultsframe, text = "-")
 bfspath.grid(row = 2, column = 2)
 bfstime = CTkLabel(resultsframe, text = "-")
 bfstime.grid(row = 2, column = 3)
-
+bfscost = CTkLabel(resultsframe, text = "-")
+bfscost.grid(row = 2, column = 4)
 
 dijkvisit = CTkLabel(resultsframe, text = "-")
 dijkvisit.grid(row = 3, column = 1)
@@ -363,6 +541,8 @@ dijkpath = CTkLabel(resultsframe, text = "-")
 dijkpath.grid(row = 3, column = 2)
 dijktime = CTkLabel(resultsframe, text = "-")
 dijktime.grid(row = 3, column = 3)
+dijkcost = CTkLabel(resultsframe, text = "-")
+dijkcost.grid(row = 3, column = 4)
 
 avisit = CTkLabel(resultsframe, text = "-")
 avisit.grid(row = 4, column = 1)
@@ -370,6 +550,8 @@ apath = CTkLabel(resultsframe, text = "-")
 apath.grid(row = 4, column = 2)
 atime = CTkLabel(resultsframe, text = "-")
 atime.grid(row = 4, column = 3)
+acost = CTkLabel(resultsframe, text = "-")
+acost.grid(row = 4, column = 4)
 
 feedback = CTkLabel(controlpanel, text = "", font = ("Arial", 12), fg_color = "transparent")
 feedback.pack(pady = (20,10))
